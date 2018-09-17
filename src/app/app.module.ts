7de8d0ba501424af24b98c45f3ca8de9556a052c
thahirakram <thahirakram289@gmail.com>
2018-09-17 09:20:37 +0530

    linking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from "@angular/router"
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
 import { TableComponent } from './table/table.component';
 import { FooterComponent } from './footer/footer.component';
 
+const rooter: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'table',
+    component: TableComponent
+  }
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +26,8 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rooter)
   ],
   providers: [],
   bootstrap: [AppComponent]
